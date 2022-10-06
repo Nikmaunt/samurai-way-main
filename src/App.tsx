0d08e9a92aca305a16f0c5a2634a8a19c31c0064
Nick <nikmaunt@gmail.com>
2022-10-06 16:07:13 +0300

    new components (import-export lesson 9 )

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./Technologies";
+import Header from "./Header";
+
 
 const App = () => {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
-}
+      <div>
+      <Header/>
+     <Technologies/>
+      </div>
+  );}
+
+
 
 export default App;
